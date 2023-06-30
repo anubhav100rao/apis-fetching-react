a3432527dc7105b0678f95dc88028627e0893ff4
anubhav100rao <anubhav100rao@gmail.com>
2023-07-01 00:58:25 +0530

    updated

---
+++ src/api/constants/apiStatus.js
@@ -0,0 +1,13 @@
+export const IDLE = "IDLE";
+export const LOADING = "LOADING";
+export const SUCCESS = "SUCCESS";
+export const ERROR = "ERROR";
+export const CANCELLED = "CANCELLED";
+
+export const defaultState = [
+    IDLE,
+    LOADING, 
+    SUCCESS, 
+    ERROR, 
+    CANCELLED
+];
